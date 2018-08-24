01bd3968f1c287242b2496ea496a4cc183fb6981
Korneliu <olbiton@gmail.com>
2018-08-23 22:53:32 -0700

    added function constructor that build obj and gives 3 property

---
+++ Greetr.js
@@ -0,0 +1,12 @@
+(function(global, $)  {
+	var Greetr = function(firstName, lastName, language)	{
+		return new Greetr.init(firstName, lastName, language);
+	}
+
+	Greetr.init = function(firstName, lastName, language) {
+  	var self = this;
+  	self.firstName = firstName || '';
+  	self.lastName = lastName || '';
+  	self.language = language || '';
+	}
+}(window, jQuery));
