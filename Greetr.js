2c05c018856a467b59bd760287eb2f2c622750f8
Korneliu <olbiton@gmail.com>
2018-08-23 23:14:27 -0700

    added language and formal language

--- Greetr.js
+++ Greetr.js
@@ -3,10 +3,29 @@
 		return new Greetr.init(firstName, lastName, language);
 	}
 
+	var supportedLangs = ['en', 'es'];
+
+	var greetings = {
+		en: 'Hello',
+		es: 'Hola'
+	};
+
+	var formalGreetings = {
+		en: 'Greetings',
+		es: 'Saludos'
+
+	};
+
+	Greetr.prototype = {};
+
 	Greetr.init = function(firstName, lastName, language) {
   	var self = this;
   	self.firstName = firstName || '';
   	self.lastName = lastName || '';
-  	self.language = language || '';
+  	self.language = language || 'en';
 	}
+
+	Greetr.init.prototype = Greetr.prototype;
+
+	global.Greetr = global.G$ = Greetr;
 }(window, jQuery));
